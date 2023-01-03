e518b16d3d5b38fc03571044f46340a791c8741a
Yash Sharma <yash.sharma@infobeans.com>
2023-01-03 13:20:33 +0530

    first commit

---
+++ src/test/java/stepDefinations/Hooks.java
@@ -0,0 +1,14 @@
+package stepDefinations;
+
+import io.cucumber.java.After;
+import resources.Base;
+
+public class Hooks extends Base{
+	
+	@After()
+	public void AfterEveryScenarion() {
+		driver.close();
+		driver.quit();
+	}
+
+}
